0ab57c87e850d6e8134310329df6b6fc07d02653
leesel <30633157+leesel@users.noreply.github.com>
2020-11-28 23:51:43 -0500

    Update ducky.js

--- ducky.js
+++ ducky.js
@@ -56,4 +56,52 @@ window.onload = function() {
   //     direction the duck is flying and change the way the duck is facing
 
   // Done, you have accomplish another level of skill
+  
+      const duck = document.createElement('div');
+    duck.className = 'duck';
+    duck.style.top = Math.random() * window.innerWidth + 'px';
+    duck.style.left = Math.random() * window.innerHeight + 'px';
+    document.body.appendChild(duck);
+    setTimeout(function() { duck.remove() }, 1)
+
+    function createDuck() {
+        const duck = document.createElement('div');
+        duck.className = 'duck';
+        duck.style.top = Math.random() * window.innerWidth + 'px';
+        duck.style.left = Math.random() * window.innerHeight + 'px';
+        document.body.appendChild(duck);
+        let flapper = setInterval(function flapDuck() {
+            duck.classList.toggle('flap');
+        }, 250);
+
+        function moveDuck() {
+            let top = Math.random() * window.innerWidth;
+            let left = Math.random() * window.innerHeight;
+
+            duck.style.top = top + 'px';
+            duck.style.left = left + 'px';
+
+            function addShot() {
+                duck.classList.add('shot');
+                setTimeout(function() { duck.remove(); checkForWinner() }, 1000)
+            }
+
+            duck.addEventListener('click', addShot);
+        }
+
+        moveDuck()
+        let mover = setInterval(moveDuck, 1000);
+        return duck;
+    };
+
+    for (let i = 0; i < 5; i++) {
+        createDuck();
+    }
+
+    function checkForWinner() {
+    if (document.getElementsByClassName('duck').length == 0) {
+        alert('You win!');
+        createDuck();
+      }
+    }
 };
